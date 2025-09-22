6d41a8293cc0c818aa146468f0001ea258f63f43
Умид Олимжанов <umidolimzanov@Mac-mini-Umid.local>
2025-09-22 12:58:41 +0500

    main view: fix add appointment button (no logic yet)

--- src/main/kotlin/uz/tonexus/ztalimcrm/view/main/MainView.kt
+++ src/main/kotlin/uz/tonexus/ztalimcrm/view/main/MainView.kt
@@ -1,14 +1,13 @@
 package uz.tonexus.ztalimcrm.view.main
 
+import com.vaadin.flow.component.ClickEvent
 import com.vaadin.flow.router.Route
 import io.jmix.flowui.app.main.StandardMainView
-import io.jmix.flowui.component.checkboxgroup.JmixCheckboxGroup
-import io.jmix.flowui.component.grid.DataGrid
 import io.jmix.flowui.kit.component.button.JmixButton
+import io.jmix.flowui.view.Subscribe
 import io.jmix.flowui.view.ViewComponent
 import io.jmix.flowui.view.ViewController
 import io.jmix.flowui.view.ViewDescriptor
-import uz.tonexus.ztalimcrm.entity.Appointment
 
 @Route("")
 @ViewController(id = "MainView")
@@ -17,4 +16,9 @@ open class MainView : StandardMainView() {
 
     @ViewComponent
     private lateinit var addAppointmentButton: JmixButton
+
+    @Subscribe(id = "addAppointmentButton", subject = "clickListener")
+    private fun onAddAppointmentButtonClick(event: ClickEvent<JmixButton>) {
+        
+    }
 }
